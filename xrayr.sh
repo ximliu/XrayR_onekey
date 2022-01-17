89d07c65225e6ad87b21798460c25130c0d7af93
Napoléon Bonaparte <ximliu0228aicmz@gmail.com>
2022-01-17 21:54:52 +0800

    Update xrayr.sh

--- xrayr.sh
+++ xrayr.sh
@@ -163,7 +163,7 @@ config_docker() {
 version: '3'
 services: 
   xrayr: 
-    image: crackair/xrayr:0.7.3
+    image: crackair/xrayr:latest
     volumes:
       - ./config.yml:/etc/XrayR/config.yml # 映射配置文件夹
       - ./dns.json:/etc/XrayR/dns.json 
